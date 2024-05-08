9de71f392e7bac449eb390e9a03ba71882d4ef1b
bx_sa1 <bms110403@gmail.com>
2024-05-09 00:45:34 +0100

    minified

--- footer.js
+++
@@ -1,6 +0,0 @@
-const x = new XMLHttpRequest();
-x.onload = function () {
-  document.getElementById("footer").innerHTML = this.responseText;
-}
-x.open("GET", "hidden/footer.html", true);
-x.send();
--- header.js
+++
@@ -1,6 +0,0 @@
-const x = new XMLHttpRequest();
-x.onload = function () {
-  document.getElementById("header").innerHTML = this.response;
-}
-x.open("GET", "hidden/header.html", true);
-x.send();
